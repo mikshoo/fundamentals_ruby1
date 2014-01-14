986bb344c98b213785864bf514a8d742e677075b
mikshoo <mikshoo@gmail.com>
2014-01-14 18:14:21 +0100

    exercise3.rb

--- exercise3.rb
+++ exercise3.rb
@@ -4,4 +4,5 @@ puts "tuesday" + 7.to_s
 #adding strings to integers
 puts "the result of 45628 multiplied by 7839 is: #{45628*7839}"
 #outputting the result in a sentence 
-
+puts (true && false) || (false && true) || !(false && false)
+# the expression equals true
