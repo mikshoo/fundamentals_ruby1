2dfe1810f9272d20028bc5bfc426cfe214199f01
mikshoo <mikshoo@gmail.com>
2014-01-14 17:03:59 +0100

    exercise3.rb

---
+++ exercise2.rb
@@ -0,0 +1,8 @@
+a = 20
+b = 65
+
+puts "The value of a is #{a}." # The value of a is 20.
+puts "The vaule of a is #{b}." # The value of a is 65.
+puts "The value of a plus b is #{a + b}." # The value of a plus b is 85.
+
+
