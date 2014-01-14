f8f7b44317a0dac058c8a28049872c59f8f0353e
mikshoo <mikshoo@gmail.com>
2014-01-14 18:38:45 +0100

    exercise4.rb

--- exercise4.rb
+++ exercise4.rb
@@ -1,3 +1,9 @@
 puts "what is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+puts "Hi how old are you?"
+age = gets.chomp.to_i
+time = Time.new
+currentyear = time.year
+puts "born in the year #{currentyear - age}." #shows what year you were born in
+
